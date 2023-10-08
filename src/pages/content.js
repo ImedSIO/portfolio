2e7072dcb5ccf83a90659380b8efbf4ac243b31f
unknown <isimaoui@gmail.com>
2023-10-08 19:10:35 +0200

    ajout des fichiers

---
+++ src/pages/content.js
@@ -0,0 +1,110 @@
+// Personal Information --> #hero section
+export const name = "Simaoui Imed"
+export const designation = "Etudiant en BTS SIO"
+export const location = "Amiens, France"
+export const website = "Rien pour l'instant"
+
+// About
+export const about = "Je suis Simaoui Imed un étudiant en BTS SIO (Service informatique au organisations), et j'apprécie la programmation et le réseau."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "2021",
+		to: "2021",
+		role: "Stagiaire en Administration Réseau",
+		org: "Konecta (Ex Comdata)",
+		org_url: "https://www.konecta-group.com/fr/",
+		location: "Amiens, France",
+		skills: "Gérer les appareils de l’entreprise, Gérer les réseaux et installations d’os sur les postes de travail, virtualisation (linux console)."
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "SR Developpement",
+		description: "Agence de developpement en Free-lance avec un camarade.",
+		url: "https://www.instagram.com/sr_dev80/",
+		tech: "Developpement logiciel et web.",
+	},
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2023",
+		to: "2025",
+		institute: "Lycée Edouard Gand",
+		course: "BTS SIO (services informatique au organisations",
+		location: "Amiens, France"
+	},
+	{
+		from: "2018",
+		to: "2021",
+		institute: "Lycée Edouard Branly",
+		course: "BAC SN option RISC (réseaux informatique systèmes communicants",
+		location: "Amiens, France"
+	},
+
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "Email",
+		source: "isimaoui@edouardgand.fr",
+	},
+	{
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/imed-simaoui-98a53b249/",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	/*
+	{
+		issued: "2005",
+		title: "Certified Safety Inspector",
+		org: "National Safety Association",
+		url: "https://certi.com/url",
+		description: "Certification in safety inspection and risk assessment.",
+	},*/
+]
+
+// Blogs --> #blogs section
+export const blogs = [
+	{
+		date: "08/10/2023",
+		title: "Progression langage de programmation",
+		description: "Actuellement j'apprend le C++",
+		publisher: "Imed",
+	},
+
+]
+
+// Achievements --> #achievement section
+export const achievements = [
+	/*{
+		title: "Safety Hero Award",
+		year: "2020",
+		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+	},
+	{
+		title: "Safety Innovation Prize",
+		year: "2018",
+		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
+	},
+	{
+		title: "Community Safety Ambassador",
+		year: "2015",
+		decription: "Honored for promoting safety awareness and education in the Quahog community."
+	},
+	{
+		title: "Safety Campaign Excellence",
+		year: "2013",
+		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+	}*/
+]
