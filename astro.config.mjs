81ba66a5ed1bd51a83b09f7223c32e3713c0e23f
unknown <isimaoui@gmail.com>
2023-10-08 21:31:54 +0200

    ajout des fichiers

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,8 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
+  export default defineConfig({
+    site: 'https://imedsio.github.io/Portfolio/',
+    base: '/my-repo',
+  })
   }),
-});
